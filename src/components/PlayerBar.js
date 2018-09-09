7f05ac2a1be237a9217048070004792c21d1fdf3
Matt Frears <matt.frears@gmail.com>
2018-09-09 15:53:04 -0600

    Assignment 8 complete

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -5,13 +5,13 @@ class PlayerBar extends Component {
     return (
       <section className="player-bar">
         <section id="buttons">
-          <button id="previous">
-            <span className="ion-skip-backward" onClick={this.props.handlePrevClick}></span>
+          <button id="previous"  onClick={this.props.handlePrevClick}>
+            <span className="ion-skip-backward"></span>
           </button>
           <button id="play-pause" onClick={this.props.handleSongClick} >
             <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
           </button>
-          <button id="next">
+          <button id="next" onClick={this.props.handleNextClick}>
             <span className="ion-skip-forward"></span>
           </button>
         </section>
